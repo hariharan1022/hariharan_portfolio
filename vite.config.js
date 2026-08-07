9be60227c5881b85853cbe51140bbcffca95f7dd
hariharan <hariharanmahesh34@gmail.com>
2026-08-07 12:26:59 +0530

    fix: make Vite base path conditional for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGithubPages = process.env.BASE_PATH || process.env.npm_lifecycle_event === 'deploy';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hariharan_portfolio/',
+  base: isGithubPages ? '/hariharan_portfolio/' : '/',
 })
